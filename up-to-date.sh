c7f751b40e123aa8b1f708701af6d57157a857c8
Andrei Pachtarou <andrei.pachtarou@gmail.com>
2015-12-27 12:11:18 +0300

    add up to day notes

---
+++ up-to-date.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#CONFIGURE upstream
+#$git remote -v
+#origin  https://github.com/YOUR_USERNAME/YOUR_FORK.git (fetch)
+#origin  https://github.com/YOUR_USERNAME/YOUR_FORK.git (push)
+
+#$git remote add upstream https://github.com/ORIGINAL_OWNER/ORIGINAL_REPOSITORY.git
+
+#$git remote -v
+#origin    https://github.com/YOUR_USERNAME/YOUR_FORK.git (fetch)
+#origin    https://github.com/YOUR_USERNAME/YOUR_FORK.git (push)
+#upstream  https://github.com/ORIGINAL_OWNER/ORIGINAL_REPOSITORY.git (fetch)
+#upstream  https://github.com/ORIGINAL_OWNER/ORIGINAL_REPOSITORY.git (push)
+
+
+#FETCH/MERGE upstream to local
+#git fetch upstream
+#git checkout master
+#git merge upstream/master
+#git merge upstream/master
+
+#PUSH to own origin
+#git push origin
